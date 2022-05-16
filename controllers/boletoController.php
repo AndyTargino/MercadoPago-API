dc365e0deb2d1a4293be3d563df469c6ac0d7165
Anderson Targino <84283346+AndyTargino@users.noreply.github.com>
2022-05-16 08:51:03 -0300

    Update boletoController.php

--- controllers/boletoController.php
+++ controllers/boletoController.php
@@ -30,13 +30,14 @@ $url_boleto = $payment->transaction_details->external_resource_url; //url do bol
 $id = $payment->id; //Id do pagamento (Necessário para verificar status do pagamento desta transação)
 $status = $payment->status; //Status atual do pagamento
 //O boleto sempre terá status como pendente assim que for gerado, diferente do cartao de credito que terá dois estados -> pending, accept
+$data['forma_pagamento'] = 'Boleto';
 if ($status == 'pending') {
     $status = 'Aguardando Pagamento';
     $msg = "<p>Obrigado, <strong>" . $data['payer']['first_name'] . "</strong>!</p> <p>O status atual do seu pagamento via <strong>" . $data['forma_pagamento'] . "</strong> é <strong>" . $status . "</strong> e o código da transação junto ao MercadoPago é <strong>" . $id . "</strong>.</p> <p>O valor total é de <strong>" . $formated_total . ". </strong><a target='new' href=" . $url_boleto . ">Clique aqui</a> para imprimir o boleto.</p>";
     $response = (['status' => $status, 'status_detail' => $payment->status_detail, 'id' => $payment->id, 'mensagem' => $msg, 'boleto' => $url_boleto]);
     echo json_encode($response);
 } else {
-    $msg = "<p>Ocorreu um erro ao gerar o boleto..</p>";
+    $msg = "<p>Ocorreu um erro ao gerar o boleto.</p>";
     $response = (['status' => 400, 'mensagem' => $msg]);
     echo json_encode($response);
 }
