0f43f06d72d8124eed9bbaaa7741f5a1a22eee45
Anderson Targino <84283346+AndyTargino@users.noreply.github.com>
2022-05-30 06:09:48 -0300

    V1.5

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 //information from https://www.mercadopago.com.br/developers/panel/credentials?id=1261619918742645
 //your public key teste
-define('SAND_KEY', 'TEST-f424cfbd-32b8-4374-9c06-a2fada5b239e');
+define('SAND_KEY', '');
 //your key teste
-define('SAND_TOKEN', 'TEST-4852070479655300-050713-eb7973693c745f82213b1c8c1621747b-227619478');
+define('SAND_TOKEN', '');
 //your public key
 define('PROD_KEY', '');
 //your key
