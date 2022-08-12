7090504e3e7ae07000153472b4a379a2246e102b
Anderson Targino <84283346+AndyTargino@users.noreply.github.com>
2022-08-12 13:16:41 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,14 @@
 <?php
 //information from https://www.mercadopago.com.br/developers/panel/credentials?id=1261619918742645
-//your public key teste
+//Sua chave publica teste
 define('SAND_KEY', '');
-//your key teste
+//Sua chave teste
 define('SAND_TOKEN', '');
-//your public key
+//Sua chave publica producao
 define('PROD_KEY', '');
-//your key
+//Sua chave producao
 define('PROD_TOKEN', '');
+
+//Seus ID`s
 define('CLI_ID', '');
 define('CLI_SECRECT', '');
